69df2db2276334eb2fd8e7853ca7e7391dec1940
zotovDmitryY214 <91723068+zotovDmitryY214@users.noreply.github.com>
2021-12-14 20:33:27 +0300

    Update 4.2.py

--- Занятие 4/4.2.py
+++ Занятие 4/4.2.py
@@ -1,9 +1,7 @@
 # -- coding: utf-8 --
 def f():
     print("Введите строку")
-    s = str(input())
-    a = len(s) // 2 + len(s) % 2
-    b = (s[a:] + s[:a])
-    return b
-print(f())
+    s = input()
+    print(s.count(" ") + 1)
 
+f()
