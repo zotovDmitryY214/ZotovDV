a0268a1032eeb63b259d3cd9e3cbd4822e7bc859
zotovDmitryY214 <91723068+zotovDmitryY214@users.noreply.github.com>
2021-12-14 20:29:06 +0300

    Add files via upload

---
+++ Занятие 5/5.8.py
@@ -0,0 +1,20 @@
+# -- coding: utf-8 --
+def f():
+    c = 1
+    ms = 0
+    lc = 0
+    while True:
+        x = int(input("Введите число - "))
+        if x == 0:
+            break
+        if (lc != 0):
+            if (x == container):
+                c += 1
+            else:
+                if (ms < c):
+                    ms = c
+                counter = 1
+        container = x
+        lc += 1
+    print(max(ms, c))
+f()
